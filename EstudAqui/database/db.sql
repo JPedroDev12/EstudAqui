c0a95d44a4dead16fa216621b20fb97a42a75883
JPedroDev12 <joaopedromes12@gmail.com>
2026-06-22 16:22:14 -0300

    Adiciona database

---
+++ EstudAqui/database/db.sql
@@ -0,0 +1,37 @@
+DROP DATABASE IF EXISTS EstudAqui;
+CREATE DATABASE EstudAqui;
+USE EstudAqui;
+
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(100) NOT NULL,
+    cargo ENUM('Aluno', 'Professor', 'Administrador') DEFAULT 'Aluno',
+    foto VARCHAR(255) DEFAULT NULL
+);
+
+CREATE TABLE categorias (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE cursos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario_id INT NOT NULL,
+    categoria_id INT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    modalidade ENUM('Presencial', 'Online') NOT NULL,
+    descricao TEXT,
+    capa VARCHAR(255) DEFAULT NULL,
+    FOREIGN KEY (usuario_id)   REFERENCES usuarios(id),
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+CREATE TABLE matriculas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario_id INT NOT NULL,
+    curso_id INT NOT NULL,
+    data_matricula TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (curso_id)   REFERENCES cursos(id)
+);
